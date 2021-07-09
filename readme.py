a2af0bda21cb1d80158bc8b37dee35935acded0c
lederpp <cbb786@gmail.com>
2021-07-08 21:17:26 -0400

    Commit

--- readme.py
+++ readme.py
@@ -1,1 +1,15 @@
 # repo contains majority of practice exercises from Introduction To Programming Using Python (1st edition) By Liang
+# and was created in spite of my only 8 week programming class expecting me to absorb
+# all of this in that little time. I voluntaritly chose to take this class.
+# the professsor is good, as bad as i feel i did on the midterm i still feel like
+# i have actually retained some of this material. its just too much in too little time.
+# im slow on the uptake to begin with, but once past that i dont have limits.
+# i took this class becasue i'm fucking determined to learn this side of tech, or at least
+# enough to say confidently that i know it to a degree. it is fun, it makes you really think
+# about problems the way you never have before.
+# so now, after the midterm, this is my fuck you to programming.
+# it wont beat me as cliche and lame as that sounds.
+# i am now, in my free time, going through this fucking book and doing every single exercise
+# Professor is awesome, and very experienced, but this way of thinking about things is soo new to me.
+# it cant be done in 8 weeks, at least for me.
+# heres to the properly titled project/repository, Round #2
