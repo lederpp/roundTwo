358ed470f34832e676ce53ff5852f5509feb0a3b
lederpp <cbb786@gmail.com>
2021-07-08 23:00:44 -0400

    Commit

---
+++ 1.13.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.forward(50)
+turtle.penup()
+turtle.goto(0, 0)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(50)
+turtle.penup()
+turtle.goto(0, 0)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(50)
+turtle.penup()
+turtle.goto(0, 0)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(50)
+turtle.penup()
+turtle.goto(0, 0)
+
+turtle.hideturtle()
+turtle.done()
---
+++ 1.14.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.left(120)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+
+
+turtle.hideturtle()
+turtle.done()
