e8422e19514dc03a9ea5d65ade3c1a0697bc9cdb
lederpp <cbb786@gmail.com>
2021-07-09 19:53:13 -0400

    Commit

---
+++ 2.2.py
@@ -0,0 +1,7 @@
+radius, length = eval(input("Enter the radius and length of a cylinder: "))
+
+area = radius * radius * 3.14159
+volume = area * length
+
+print("The area of the cylinder is: ", area)
+print("The volume of the cylinder is: ", volume)
