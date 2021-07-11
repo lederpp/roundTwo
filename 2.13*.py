26c65300a5fc59eaab298b3b3c5fa74b7704a605
lederpp <cbb786@gmail.com>
2021-07-11 14:32:16 -0400

    Commit

---
+++ 2.13*.py
@@ -0,0 +1,10 @@
+# Look over, was on exam
+
+w, x, y, z = eval(input("Enter a four digit integer(each didgit seperated by a comma): "))
+w, x, y, z = z, y, x, w
+
+print(w, x, y, z)
+
+# Differnt solution
+
+w, x, y, z = eval(input("Enter a four digit integer: "))
