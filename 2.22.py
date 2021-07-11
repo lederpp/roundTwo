096dba7e5210fe1f53c5f7e9e14a651d38d850b4
lederpp <cbb786@gmail.com>
2021-07-11 17:59:50 -0400

    Commit

---
+++ 2.22.py
@@ -0,0 +1,10 @@
+
+years = eval(input("Enter the number of years: "))
+
+currentPop = 312032486
+secYear = 365 * 24 * 60 * 60        # Seconds in year *****
+
+growthPerYear = currentPop + (years * secYear // 7) - (years * secYear // 13) + (years * secYear // 45)
+
+
+print("The population in ", years, "years is ", growthPerYear)
