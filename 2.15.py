88f520505a4d17fe207bbaf9eeafe8858beb7c8f
lederpp <cbb786@gmail.com>
2021-07-11 16:02:51 -0400

    Commit

---
+++ 2.15.py
@@ -0,0 +1,6 @@
+
+s = eval(input("Enter the side of a hexagon: "))
+
+area = ((3 * (3 ** 0.5) / 2) * (s ** 2))
+
+print("The area of the hexagon is: ", area)
---
+++ 2.16.py
@@ -0,0 +1,6 @@
+
+v0, v1, t = eval(input("Enter v0, v1, and t (separated by commas): "))
+
+a = (v1 - v0) / t
+
+print("The average acceleration is: ", a)
