4e33c5aff6fef17c5d44733a1a79706115caad71
lederpp <cbb786@gmail.com>
2021-07-19 23:52:27 -0400

    Commit

--- 3.6*.py
+++ 3.6*.py
@@ -1,5 +1,5 @@
 # (Find the character of an ASCII code)
 
-c = int(input("Enter an integer between 0 & 127: "))
-c = chr(c)
-print("The character is: ", c)
+c = eval(input("Enter an integer between 0 & 127: "))
+
+print(c, chr(c))
